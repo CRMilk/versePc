b76043fc8548b2e5318a0028136d460431f4b4ba
doujie2978166201-spec <8gwjcv2fxs@privaterelay.appleid.com>
2026-06-06 09:19:56 +0800

    修复NSIS安装包文件解压详情显示（修补common.nsh ShowInstDetails）并显示安装大小524MB

--- fix_port.ps1
+++
@@ -1,8 +0,0 @@
-﻿$path = "E:\Verse Explorer X\VersePC\agent-engine.js"
-$content = [System.IO.File]::ReadAllText($path)
-$modified = $content.Replace("hostname: apiUrl.hostname,
-        path: apiUrl.pathname", "hostname: apiUrl.hostname,
-        port: apiUrl.port || undefined,
-        path: apiUrl.pathname")
-[System.IO.File]::WriteAllText($path, $modified)
-Write-Output "DONE"
